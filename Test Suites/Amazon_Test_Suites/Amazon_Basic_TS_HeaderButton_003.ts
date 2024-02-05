a948e9c85a6dd9ca4366cd184ab0401d375e73ef
Rohit Ravindrakumar Bagal <69656774+RohitBagal@users.noreply.github.com>
2024-02-05 22:28:51 +0530

    Add files via upload

---
+++ Test Suites/Amazon_Test_Suites/Amazon_Basic_TS_HeaderButton_003.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_Basic_TS_HeaderButton_003</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6642d46a-206e-4573-9275-72eaad5fe534</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e5dc2fd-fbbc-46b5-8614-30d6de07b827</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_Test Case/Amazon_TC_SignIn_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3379071c-697d-40b6-ac93-00dbb32aa8e2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>497c29f6-8d2a-4e43-818e-c4003764dabf</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b503c884-b1da-419c-a250-4054187e473f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_Test Case/Amazon_TC_SearchItem_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d79e24d9-c919-49d7-b932-74210bdd3f85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_Test Case/Amazon_TC_HeaderButton_003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Amazon_Test_Suites/Amazon_Basic_TS_Searchitem_002.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_Basic_TS_Searchitem_002</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e74aa545-4d77-4f7c-bd76-ed82124995a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>533819f6-6706-41ec-b413-5a47df787337</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon_Test Case/Amazon_TC_SearchItem_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
